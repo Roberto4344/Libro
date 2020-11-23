13abf2589b78d88c72e65243fa3c3b10326bafec
Roberto <robertoabad93@gmail.com>
2020-11-23 09:03:38 +0100

    1847.05 se ha añadido metodo getter y setter para el numero de referencia  el cual no puede ser modificado si tiene menos de 3 digitos

--- Libro.java
+++ Libro.java
@@ -13,46 +13,68 @@ public class Libro {
     private String autor;
     private String titulo;
     private int paginas;
-     
+    private String numRef;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro,int numeroPaginas)
-    {
+    public Libro(String autorLibro, String tituloLibro,int numeroPaginas){
         autor = autorLibro;
         titulo = tituloLibro;
         paginas = numeroPaginas;
+        numRef =" ";
     }
-    
+
     public String getAutor(){
         return autor;
     }
-    
+
     public String getTitulo(){
         return titulo;
     }
-    
+
     public int getPaginas(){
         return paginas;
     }
-    
+
+    public String getnRef(){
+        return numRef;
+    }
+
+   /**
+     * setter del numero de referencia
+     */
+    public void setNRef(String newNRef){
+        if( newNRef.length() < 3){
+            System.out.println("Numero no valido ,requiere de un minimo de 3 caracteres, el atributo no se modificara"); 
+        }
+        else{
+            this.numRef = newNRef;
+        }   
+    }
+
     public void impAutor(){
         System.out.println("El autor es "+ autor);
     }
-    
+
     public void impTitulo(){
         System.out.println("El titulo del libro es "+titulo);
     }
-    
+
     public void impDetalles(){
-        System.out.println("Autor:" + autor + ",Libro:" + titulo + ",N PAginas: " + paginas);
+        if (numRef == " "){
+            numRef = "ZZZ";
+        }
+        System.out.println("Autor:" + autor + ",Libro:" + titulo + ",N PAginas: " + paginas + "Numero Referencia:"+numRef);
     }
-    
+
     public String getDetalles(){
         String detalles;
         detalles ="";
-        detalles =(detalles +" Autor: "+ autor + " Libro: "+ titulo + " Paginas:"+ paginas);
+        if (numRef == " "){
+            numRef = "ZZZ";
+        }
+        detalles =detalles +" Autor: "+ autor + " Libro: "+ titulo + " Paginas:"+ paginas +" Numero referencia:"+ numRef;
         return detalles;
     }
 }
